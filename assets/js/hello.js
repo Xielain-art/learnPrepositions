839486882ca32e799cbd8846479366083653d7d1
Xielain <rklimow409@gmail.com>
2023-04-15 01:12:14 +0300

    hello page done

---
+++ assets/js/hello.js
@@ -0,0 +1,42 @@
+const robot = document.querySelector("#robot");
+const helloText = document.querySelector(".hello__text");
+const helloBtn = document.querySelectorAll(".hello__btn");
+
+const text = helloText.innerText;
+let newHtml = "";
+
+for (const letter of text) {
+  newHtml += `<span>${letter}</span>`;
+}
+helloText.innerHTML = newHtml;
+
+let spans = helloText.querySelectorAll("span");
+let count = 0;
+let timeout = 0;
+
+function typing_text() {
+  spans[count].classList.add("visible");
+  if (spans[count].innerText == " " || spans[count].innerHTML == " ") {
+    timeout = Math.floor(Math.random() * Math.floor(300));
+    spans[count].classList.add("cursor");
+  } else {
+    timeout = 50;
+  }
+  if (count < text.length - 1) {
+    setTimeout(() => {
+      spans[count].classList.remove("cursor");
+      count++;
+      typing_text();
+    }, timeout);
+  } else {
+    helloBtn.forEach((el) =>
+      el.classList.add("visible", "magictime", "puffIn")
+    );
+  }
+}
+
+const onLoad = function () {
+  robot.classList.add("magictime", "puffIn");
+  setTimeout(typing_text, 1000);
+};
+window.addEventListener("load", onLoad);
